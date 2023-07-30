612ce74627a30e8319c6ceab1b1323d632c3d959
luisec3 <140930341+luisec3@users.noreply.github.com>
2023-07-30 07:40:37 +0000

    displayed dynamic data in the html

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 
+PROJECTS = [
+  {
+    'id': 1,
+    'title': 'Agente viajero con algoritmo de colonia de hormigas',
+    'topic': 'Bioinspired algorithms',
+    'programming_language': 'Python'
+  },
+  
+  {
+    'id': 2,
+    'title': 'Geometric transformation of images',
+    'topic': 'Image processing',
+    'programming_language': 'Python'
+  }
+]
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', projects=PROJECTS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
